0cde13d7d7153bfc2815253c26912ab7ce4d5922
Jona Buck <Jonabuck2@gmail.com>
2020-12-24 14:24:34 +0100

    Many Improvements
    -BIG PCB improvements
    - Programm more stable
    added Features like Dimming and Updates
    - Improved Frontplate

--- Firmware/Wordclock/src/main.cpp
+++ Firmware/Wordclock/src/main.cpp
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 #include <WiFi.h>
 #include <WebServer.h>
 typedef WebServer WiFiWebServer;
@@ -6,10 +7,11 @@ typedef WebServer WiFiWebServer;
 #include "time.h"
 
 
-
-
 #define NUM_LEDS 114
 #define DATA_PIN 23
+#define lightSensor 34
+//#define touch_pin_numer T0
+
 
 WiFiWebServer server;
 AutoConnect portal(server);
@@ -19,40 +21,34 @@ AutoConnectConfig config;
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 3600;
 const int   daylightOffset_sec = 3600;
+int brightnessvalue = 800;
+int lightstate;
+
+
+
+
 
 // Zeit variablen
 int led = 0;
 int tm_min;
 int tm_min_old = 99;
 int tm_hour;
-int tm_hour_old = 99;
-int R = 50;
-int G = 50;
-int B = 30;
+int H = 0;
+int S = 0;
+int V = 160;
 CRGB leds[NUM_LEDS];
 
 
-void printLocalTime()
-{
-  struct tm timeinfo;
-  if (!getLocalTime(&timeinfo)) {
-    // when failed to obtain time
-    return;
-  }
-
-  tm_min = timeinfo.tm_min;
-  tm_hour = timeinfo.tm_hour;
-
-}
 
 void getTime()
 {
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
   delay(1000);
-  printLocalTime();
 
   struct tm timeinfo;
   if (!getLocalTime(&timeinfo)) {
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+    delay(4000);
     configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
     delay(3000);
     if (!getLocalTime(&timeinfo)){
@@ -63,7 +59,17 @@ void getTime()
   }
 }
 
+void printLocalTime()
+{
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    getTime();
+  }
 
+  tm_min = timeinfo.tm_min;
+  tm_hour = timeinfo.tm_hour;
+
+}
 
 
 void displayTime() {
@@ -72,33 +78,33 @@ void displayTime() {
   switch (tm_min % 5) { // Punkte min
 
     case 0:
-      leds[110].setRGB( 0, 0, 0);
-      leds[111].setRGB( 0, 0, 0);
-      leds[112].setRGB( 0, 0, 0);
-      leds[113].setRGB( 0, 0, 0);
+      leds[110].setHSV( 0, 0, 0);
+      leds[111].setHSV( 0, 0, 0);
+      leds[112].setHSV( 0, 0, 0);
+      leds[113].setHSV( 0, 0, 0);
 
       break;
 
     case 1:
-      leds[110].setRGB( R, G, B);
+      leds[110].setHSV( H, S, V);
       break;
 
     case 2:
-      leds[110].setRGB( R, G, B);
-      leds[111].setRGB( R, G, B);
+      leds[110].setHSV( H, S, V);
+      leds[111].setHSV( H, S, V);
       break;
 
     case 3:
-      leds[110].setRGB( R, G, B);
-      leds[111].setRGB( R, G, B);
-      leds[112].setRGB( R, G, B);
+      leds[110].setHSV( H, S, V);
+      leds[111].setHSV( H, S, V);
+      leds[112].setHSV( H, S, V);
       break;
 
     case 4:
-      leds[110].setRGB( R, G, B);
-      leds[111].setRGB( R, G, B);
-      leds[112].setRGB( R, G, B);
-      leds[113].setRGB( R, G, B);
+      leds[110].setHSV( H, S, V);
+      leds[111].setHSV( H, S, V);
+      leds[112].setHSV( H, S, V);
+      leds[113].setHSV( H, S, V);
       break;
 
     default:
@@ -113,15 +119,15 @@ void displayTime() {
 
     case 0:
       for (int i = 7; i < 11; i++) { //Fünf
-        leds[i].setRGB( 0, 0, 0);
+        leds[i].setHSV( 0, 0, 0);
       }
 
       for (int i = 41; i < 44; i++) { //Vor
-        leds[i].setRGB( 0, 0, 0);
+        leds[i].setHSV( 0, 0, 0);
       }
 
       for (int i = 99; i < 102; i++) { //Uhr
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
 
 
@@ -129,71 +135,71 @@ void displayTime() {
 
     case 1:
       for (int i = 99; i < 102; i++) { //Uhr
-        leds[i].setRGB( 0, 0, 0);
+        leds[i].setHSV( 0, 0, 0);
       }
 
       for (int i = 7; i < 11; i++) { //Fünf
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
 
       for (int i = 33; i < 37; i++) { //Nach
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
       break;
     case 2:
       for (int i = 18; i < 22; i++) { // Zehn
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
       for (int i = 33; i < 37; i++) { //Nach
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
       for (int i = 7; i < 11; i++) { //Fünf
-        leds[i].setRGB( 0, 0, 0);
+        leds[i].setHSV( 0, 0, 0);
       }
       break;
 
     case 3:
       for (int i = 18; i < 22; i++) { // Zehn
-        leds[i].setRGB( 0, 0, 0);
+        leds[i].setHSV( 0, 0, 0);
       }
 
       for (int i = 26; i < 33; i++) { //Viertel
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
       for (int i = 33; i < 37; i++) { //Nach
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
 
       break;
 
     case 4:
       for (int i = 11; i < 18; i++) { //Zwanzig
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
       for (int i = 33; i < 37; i++) { //Nach
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
 
       for (int i = 26; i < 33; i++) { //Viertel
-        leds[i].setRGB( 0, 0, 0);
+        leds[i].setHSV( 0, 0, 0);
       }
       break;
 
     case 5:
       for (int i = 33; i < 37; i++) { //Nach
-        leds[i].setRGB( 0, 0, 0);
+        leds[i].setHSV( 0, 0, 0);
       }
       for (int i = 11; i < 18; i++) { //Zwanzig
-        leds[i].setRGB( 0, 0, 0);
+        leds[i].setHSV( 0, 0, 0);
       }
       for (int i = 41; i < 44; i++) { // Vor
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
       for (int i = 7; i < 11; i++) { //Fünf
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
       for (int i = 44; i < 48; i++) { //Halb
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
       break;
 
@@ -205,54 +211,53 @@ void displayTime() {
       for (int i = 41; i < 44; i++) { // Vor
         leds[i].setRGB(0, 0, 0);
       }
-      break;
 
       for (int i = 44; i < 48; i++) { //Halb
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
-
+      break;
     case 7:
       for (int i = 33; i < 37; i++) { //Nach
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
 
       for (int i = 7; i < 11; i++) { //Fünf
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
 
       for (int i = 44; i < 48; i++) { //Halb
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
 
       break;
 
     case 8:
       for (int i = 7; i < 11; i++) { //Fünf
-        leds[i].setRGB( 0, 0, 0);
+        leds[i].setHSV( 0, 0, 0);
       }
       for (int i = 11; i < 18; i++) { //Zwanzig
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
       for (int i = 33; i < 37; i++) { //Nach
-        leds[i].setRGB( 0, 0, 0);
+        leds[i].setHSV( 0, 0, 0);
       }
       for (int i = 44; i < 48; i++) { //Halb
-        leds[i].setRGB( 0, 0, 0);
+        leds[i].setHSV( 0, 0, 0);
       }
       for (int i = 41; i < 44; i++) { // Vor
-        leds[i].setRGB(R, G, B);
+        leds[i].setHSV( H, S, V);
       }
       break;
 
     case 9:
       for (int i = 11; i < 18; i++) { //Zwanzig
-        leds[i].setRGB( 0, 0, 0);
+        leds[i].setHSV( 0, 0, 0);
       }
       for (int i = 26; i < 33; i++) { //Viertel
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
       for (int i = 41; i < 44; i++) { // Vor
-        leds[i].setRGB(R, G, B);
+        leds[i].setHSV( H, S, V);
       }
 
 
@@ -260,28 +265,28 @@ void displayTime() {
 
     case 10:
       for (int i = 26; i < 33; i++) { //Viertel
-        leds[i].setRGB( 0, 0, 0);
+        leds[i].setHSV( 0, 0, 0);
       }
 
       for (int i = 18; i < 22; i++) { //Zehn
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
       for (int i = 41; i < 44; i++) { // Vor
-        leds[i].setRGB(R, G, B);
+        leds[i].setHSV( H, S, V);
       }
 
       break;
 
     case 11:
       for (int i = 7; i < 11; i++) { //Fünf
-        leds[i].setRGB( R, G, B);
+        leds[i].setHSV( H, S, V);
       }
 
       for (int i = 18; i < 22; i++) { //Zehn
-        leds[i].setRGB( 0, 0, 0);
+        leds[i].setHSV( 0, 0, 0);
       }
       for (int i = 41; i < 44; i++) { // Vor
-        leds[i].setRGB(R, G, B);
+        leds[i].setHSV( H, S, V);
       }
 
       break;
@@ -303,17 +308,16 @@ void displayTime() {
       else {
         tm_hour = tm_hour + 1;}
     }
-    tm_hour_old = tm_hour;
 
     switch (tm_hour) {
       case 1:
         for (int i = 94; i < 99; i++) { //Zwölf
-          leds[i].setRGB( 0, 0, 0);
+          leds[i].setHSV( 0, 0, 0);
         }
         for (int i = 63; i < 66; i++) { //Eins
-          leds[i].setRGB( R, G, B);}
+          leds[i].setHSV( H, S, V);}
           if (tm_min > 4) {
-            leds[62].setRGB( R, G, B);
+            leds[62].setHSV( H, S, V);
           }
           if (tm_min == 5){
             getTime();
@@ -322,106 +326,107 @@ void displayTime() {
         break;
       case 2:
         for (int i = 62; i < 66; i++) { //Eins
-          leds[i].setRGB( 0, 0, 0);
+          leds[i].setHSV( 0, 0, 0);
         }
         for (int i = 55; i < 59; i++) { //Zwei
-          leds[i].setRGB( R, G, B);
+          leds[i].setHSV( H, S, V);
         }
         break;
       case 3:
         for (int i = 55; i < 59; i++) { //Zwei
-          leds[i].setRGB( 0, 0, 0);
+          leds[i].setHSV( 0, 0, 0);
         }
         for (int i = 66; i < 70; i++) { //Drei
-          leds[i].setRGB( R, G, B);
+          leds[i].setHSV( H, S, V);
         }
         break;
       case 4:
         for (int i = 66; i < 70; i++) { //Drei
-          leds[i].setRGB( 0, 0, 0);
+          leds[i].setHSV( 0, 0, 0);
         }
         for (int i = 73; i < 77; i++) { //Vier
-          leds[i].setRGB( R, G, B);
+          leds[i].setHSV( H, S, V);
         }
         break;
       case 5:
         for (int i = 73; i < 77; i++) { //Vier
-          leds[i].setRGB( 0, 0, 0);
+          leds[i].setHSV( 0, 0, 0);
         }
         for (int i = 51; i < 55; i++) { //Fünf
-          leds[i].setRGB( R, G, B);
+          leds[i].setHSV( H, S, V);
         }
         break;
       case 6:
         for (int i = 51; i < 55; i++) { //Fünf
-          leds[i].setRGB( 0, 0, 0);
+          leds[i].setHSV( 0, 0, 0);
         }
         for (int i = 83; i < 88; i++) { //Sechs
-          leds[i].setRGB( R, G, B);
+          leds[i].setHSV( H, S, V);
         }
         break;
       case 7:
         for (int i = 83; i < 88; i++) { //Sechs
-          leds[i].setRGB( 0, 0, 0);
+          leds[i].setHSV( 0, 0, 0);
         }
         for (int i = 88; i < 94; i++) { //Sieben
-          leds[i].setRGB( R, G, B);
+          leds[i].setHSV( H, S, V);
         }
         break;
       case 8:
         for (int i = 88; i < 94; i++) { //Sieben
-          leds[i].setRGB( 0, 0, 0);
+          leds[i].setHSV( 0, 0, 0);
         }
         for (int i = 77; i < 81; i++) { //Acht
-          leds[i].setRGB( R, G, B);
+          leds[i].setHSV( H, S, V);
         }
         break;
       case 9:
         for (int i = 77; i < 81; i++) { //Acht
-          leds[i].setRGB( 0, 0, 0);
+          leds[i].setHSV( 0, 0, 0);
         }
         for (int i = 103; i < 107; i++) { //Neun
-          leds[i].setRGB( R, G, B);
+          leds[i].setHSV( H, S, V);
         }
         break;
       case 10:
         for (int i = 103; i < 107; i++) { //Neun
-          leds[i].setRGB( 0, 0, 0);
+          leds[i].setHSV( 0, 0, 0);
         }
         for (int i = 106; i < 110; i++) { //Zehn
-          leds[i].setRGB( R, G, B);
+          leds[i].setHSV( H, S, V);
         }
         break;
       case 11:
-        for (int i = 62; i < 66; i++) { //Zehn
-          leds[i].setRGB( 0, 0, 0);
+        for (int i = 106; i < 110; i++) { //Zehn
+          leds[i].setHSV( 0, 0, 0);
         }
         for (int i = 49; i < 52; i++) { //Elf
-          leds[i].setRGB( R, G, B);
+          leds[i].setHSV( H, S, V);
         }
         break;
       case 12:
         for (int i = 49; i < 52; i++) { //Elf
-          leds[i].setRGB( 0, 0, 0);
+          leds[i].setHSV( 0, 0, 0);
         }
         for (int i = 94; i < 99; i++) { //Zwölf
-          leds[i].setRGB( R, G, B);
+          leds[i].setHSV( H, S, V);
         }
         break;
 
     }
-  leds[0].setRGB( R, G, B);  //E
-  leds[1].setRGB( R, G, B);  //S
-  leds[3].setRGB( R, G, B);  //I
-  leds[4].setRGB( R, G, B);  //S
-  leds[5].setRGB( R, G, B);  //T
+  leds[0].setHSV( H, S, V);  //E
+  leds[1].setHSV( H, S, V);  //S
+  leds[3].setHSV( H, S, V);  //I
+  leds[4].setHSV( H, S, V);  //S
+  leds[5].setHSV( H, S, V);  //T
 
   FastLED.show();
 
 }
 
 
-void time() {
+void time()
+{
     printLocalTime();
     if (tm_min != tm_min_old) {
       tm_min_old = tm_min;
@@ -432,41 +437,63 @@ void time() {
 
 
 void setup() {
+    Serial.begin(115200);
+    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+    FastLED.clear();
+    FastLED.setCorrection( CRGB( 255, 255, 230) );
 
   // Responder of root page and apply page handled directly from WebServer class.
   server.on("/", []() {
     String content = R"(
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-Place the root page with the sketch application.&ensp;
-__AC_LINK__
-</body>
-</html>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <html>
+      <body>
+      <button onclick="window.location.href='http://wortuhr/_ac'">Wlan und Updates</button>
+      </body>
+      </html>
     )";
-    content.replace("__AC_LINK__", String(AUTOCONNECT_LINK(COG_16)));
     server.send(200, "text/html", content);
   });
+  
+  
 
   config.ota = AC_OTA_BUILTIN;
   config.autoReconnect = true;
+  config.title = "Wortuhr";
   config.hostName = "Wortuhr";
+  config.apid = "Wortuhr";
   portal.config(config);
   portal.begin();
 
-
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  //connect to WiFi
-
-  FastLED.clear();
   getTime();
   
+}
+
+ void dimmen() {
+  int light = analogRead(lightSensor);  
+  if((lightstate != 0 ) && (light < brightnessvalue))
+  {
+
+    for (int i = 255; i > 60; i--){
+      FastLED.setBrightness(i);
+      FastLED.show();
+      delay(15);
+    }
+    lightstate = 0;}
+
+  else if ((lightstate != 1 ) && (light > brightnessvalue))
+  {
+    for (int i = 60; i < 210; i++){
+    FastLED.setBrightness(i);
+    FastLED.show();
+    delay(15);}
+    lightstate = 1;
+  }
+
 }
 
 void loop() {
   portal.handleClient();
   time();
+  dimmen();
 }
